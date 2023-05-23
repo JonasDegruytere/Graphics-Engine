6084059ecd84c0b3b569e304d72eb6ae3a501e5e
Jonas <gargantuanimpact@gmail.com>
2023-05-23 21:00:32 +0200

    Readme.md

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Computer-Graphics-Engine
+## Functionality
+
+- 2D L-systems
+- 2D L-systems with brackets
+
+- Transformations
+- Eye-point
+- Projections
+
+- Platonic bodies
+- Cone and cylinder
+- Sphere
+- Torus
+
+- Z-Buffering with lines
+- Triangulation
+- Z-buffering with triangles
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/final_engine")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/engine")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Jonas/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/222.4345.21/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/Jonas/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.9011.31/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
